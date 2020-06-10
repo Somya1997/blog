d7da7397abab1f9a3f9421c157f2a75cce1d2d28
Somya1997 <43964018+Somya1997@users.noreply.github.com>
2020-06-10 14:35:35 +0530

    first

---
+++ main.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include('partials._head')
+  </head>
+  <body>
+     @include('partials._nav')
+     @include('partials._messages')
+    @yield('content')
+    <hr>
+    @include('partials._footer')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script>
+      $('ul.nav li.dropdown').hover(function() {
+     $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+        }, function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+        });
+    </script>
+    @yield('scripts')
+  </body>
+</html>
